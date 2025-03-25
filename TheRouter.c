1ebbe5b0a8caa9262075366d32daf46d2c705f10
Pirasath Luxchumykanthan <hello@how-do-i-get-your-attention.com>
2025-03-25 09:54:06 +0000

    Update on 2025-03-25 09:54:06

--- TheRouter.c
+++ TheRouter.c
@@ -1,14 +1,17 @@
 #include "../TheRouter/TheRouter.h"
 #include "../TheNetworkAdapter/TheNetworkAdapter.h"
 SetupTM(TheRouter,0,0,253,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255)
-static void SlowReviewPacket(struct ReviewPacketData *rpd){
+static void ReviewPacket(struct ReviewPacketData *rpd){
     if(!rpd)return;
-  
+
+    printk (KERN_INFO "TheRouter: SlowReviewPacket has been called.\n");
 }   
 static void TMStart(void){
+    GetTheRouterTM();
+    SetTM(TheRouterTM,ReviewPacket);
     //GetRouterTM();
     //TheRouterTM
 }
 static void TMEnd(void*data){
-    kfree(data);
+
 }
--- TheRouter.h
+++ TheRouter.h
@@ -2,9 +2,10 @@
 #ifndef TheRouter_H
 #define TheRouter_H
 #include "../TheMaintainer/TheMaintainer.h"
+struct ReviewPacketData;
 struct TheRouter{
     SetupEWB;
-    void(*SlowReviewPacket)(struct ReviewPacketData*);
+    void(*ReviewPacket)(struct ReviewPacketData*);
 };
-#define GetRouterTM()TM(TheRouter,253,255,255,255,255,255,255,255,255,255,255,255,255,255,255)
+#define GetTheRouterTM()TM(TheRouter,253,255,255,255,255,255,255,255,255,255,255,255,255,255,255)
 #endif
